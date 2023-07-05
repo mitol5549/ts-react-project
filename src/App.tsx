d1ef1148505c9e59a680aa60a65b885709959115
Andrey Osipov <mitol5549@gmail.com>
2023-07-05 13:09:36 +0200

    NewTodo Component added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
+
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
+  useState();
+
+  const addTodoHandler = (todoText: string) => {};
+
   const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
