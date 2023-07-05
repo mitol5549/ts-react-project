99c5e69a87be2801787b5f248b7316e2112b8fad
Andrey Osipov <mitol5549@gmail.com>
2023-07-05 12:04:17 +0200

    Todos Component connected.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css';
+import Todos from './components/Todos';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Todos items={['Learn React', 'Learn TypeScript']} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,3 +1,11 @@
-function Todos() {}
+const Todos: React.FC<{ items: string[] }> = props => {
+  return (
+    <ul>
+      {props.items.map(item => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default Todos;
